68663febecc883feab67605262f53eb3b02f6a8d
ElitonPenkeNot <eliton.penke@gmail.com>
2026-06-13 11:23:52 -0300

    adiciona campo 'endereco' na função 'criar_conta' e atualiza lógica de verificação de usuário

--- auth_routes.py
+++ auth_routes.py
@@ -10,17 +10,23 @@ async def home():
     return{"mensagem":"rota padrão de autenticação"}
 
 
+#codigo aonde deu certo 200
+#codgio aonde deu errado
+
 
 #estou postando informações no meu bd
 @auth_router.post("/criar_conta")
-async def criar_conta(nome:str,email:str,senha:str): #passa os parametos e o proprio fastapi vai verificar os tipos da variavel
+async def criar_conta(nome:str,email:str,senha:str,endereco=str): #passa os parametos e o proprio fastapi vai verificar os tipos da variavel
+    
     Session=sessionmaker(bind=db)
-    session=session() # aqui eu abro uma seção, porem tenho que fechar ele
+    session=Session() # aqui eu abro uma seção, porem tenho que fechar ele
+    
     usuario= session.query(user).filter(user.email==email).first() #uma query para ver se tem um user do bd igual ao meu atual tentando inserir
+    
     if usuario:
         return {"ja existe um usuario com esse email"}
     else:
-        novo_usuario= user(nome,email,senha)
+        novo_usuario= user(nome,email,senha,endereco)
         session.add(novo_usuario)
         session.commit() #comita tudo e encera a seção
         return {"Usuario cadastrado com sucesso meu chapa"}
