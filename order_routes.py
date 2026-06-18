5b309a6cbd2d683336d7ca577a660ba4bb637389
ElitonPenkeNot <eliton.penke@gmail.com>
2026-06-18 19:19:36 -0300

    adiciona rotas para criação de pedidos e define esquema de pedido

--- order_routes.py
+++ order_routes.py
@@ -1,7 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from dependecies import pegar_sessao
+from schemas import PedidoSchema 
+from models import pedido
+
+
 
 order_router = APIRouter(prefix="/pedidos", tags=['roteador_edidos']) #definindo que todas as rotas aqui vai ficar dentro de order
 
 @order_router.get("/") #ex: dominio.com/pedido
 async def pedidos():
     return {"mensagem":"acessou a rota de pedidos"}
+
+
+
+#criar um pedido em si
+@order_router.post("/pedido")
+async def criar_pedido(pedido_schema:PedidoSchema, session: Session = Depends(pegar_sessao)):
+    novo_pedido = pedido(usuario=pedido_schema.usuario)
+    session.add(novo_pedido)
+    session.commit()
+    return {"mensagem": f"pedido feito com sucesso, id do pedido {pedido_schema.usuario}"}
--- schemas.py
+++ schemas.py
@@ -3,7 +3,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
-#essa classe vai me dizer o que eu preciso passar exatamente o formato
+#essa classe vai me dizer o que eu preciso passar exatamente o formato do novo user
 class UsuarioSchema(BaseModel):
     nome:str
     email:str
@@ -13,5 +13,15 @@ class UsuarioSchema(BaseModel):
     endereco:str
     
     #crio um subclass para linkar com o meu models do db(vai ser interpretado para tranformar para SQL)
+    class Config:
+        from_attributes=True
+        
+        
+#essa classe vai me dizer o que eu preciso passar exatamente o formato de um novo pedido
+class PedidoSchema(BaseModel):
+    usuario:int   #armazena o id do usuario
+    #status vem por padrão pendente
+    #preco vem padrão, assim a unica infromação de preciso é o id do user
+    
     class Config:
         from_attributes=True
